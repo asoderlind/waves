6cfc26d9668ba4ef79fd1d9503b8dbd12997b615
asoderlind <axesode@gmail.com>
2024-11-14 12:06:23 +0100

    move arrow to other function

--- src/waves.js
+++ src/waves.js
@@ -21,7 +21,6 @@ import {
   PONG_PHASE_UNIT,
   PING_TRANSFORM_UNIT,
   PONG_TRANSFORM_UNIT,
-  UI_COLOR,
   PROFILE_AMPLITUDE,
   PROFILE_OMEGA,
   PROFILE_PHI,
@@ -30,14 +29,6 @@ import {
   PROFILE_COLOR,
   PROFILE_LINE_WIDTH,
   CHOPPINESS_SCALE,
-  ARROW_ORIGIN,
-  ARROW_SHAFT_WIDTH,
-  ARROW_HEAD_WIDTH,
-  ARROW_HEAD_HEIGHT,
-  ARROW_OFFSET,
-  WIND_SCALE,
-  MIN_WIND_SPEED,
-  MAX_WIND_SPEED,
   HANDLE_COLOR,
   SLIDER_LEFT_COLOR,
   SLIDER_RIGHT_COLOR,
@@ -90,14 +81,10 @@ import {
   MAX_AZIMUTH,
   MIN_ELEVATION,
   MAX_ELEVATION,
-  addToVector,
-  subtractFromVector,
-  multiplyVectorByScalar,
   makeIdentityMatrix,
   makeXRotationMatrix,
   makeYRotationMatrix,
   distanceBetweenVectors,
-  lengthOfVector,
   setVector4,
   projectVector4,
   transformVectorByMatrix,
@@ -110,7 +97,6 @@ import {
   buildShader,
   buildTexture,
   buildFramebuffer,
-  epsilon,
   toCSSMatrix,
   setPerspective,
   setTransformOrigin,
@@ -120,6 +106,8 @@ import {
   hasWebGLSupportWithExtensions,
   requestAnimationFrame,
 } from "./shared.js";
+
+import Arrow from "./Arrow.js";
 
 // simulation.js
 class Camera {
@@ -1036,141 +1024,6 @@ class Profile {
       context.stroke();
     };
     this.render(INITIAL_CHOPPINESS);
-  }
-}
-
-class Arrow {
-  constructor(parent, valueX, valueY) {
-    const arrow = [valueX * WIND_SCALE, 0.0, valueY * WIND_SCALE];
-    const tip = addToVector([], ARROW_ORIGIN, arrow);
-
-    const shaftDiv = document.createElement("div");
-    shaftDiv.style.position = "absolute";
-    shaftDiv.style.width = ARROW_SHAFT_WIDTH + "px";
-    shaftDiv.style.background = UI_COLOR;
-    setTransformOrigin(shaftDiv, "center top");
-    setTransform(
-      shaftDiv,
-      "translate3d(" +
-        (ARROW_ORIGIN[0] - ARROW_SHAFT_WIDTH / 2) +
-        "px, " +
-        ARROW_ORIGIN[1] +
-        "px, " +
-        ARROW_ORIGIN[2] +
-        "px) rotateX(90deg)"
-    );
-    parent.appendChild(shaftDiv);
-
-    const headDiv = document.createElement("div");
-    headDiv.style.position = "absolute";
-    headDiv.style.borderStyle = "solid";
-    headDiv.style.borderColor =
-      UI_COLOR + " transparent transparent transparent";
-    headDiv.style.borderWidth =
-      ARROW_HEAD_HEIGHT +
-      "px " +
-      ARROW_HEAD_WIDTH / 2 +
-      "px 0px " +
-      ARROW_HEAD_WIDTH / 2 +
-      "px";
-    setTransformOrigin(headDiv, "center top");
-    setTransform(
-      headDiv,
-      "translate3d(" +
-        (ARROW_ORIGIN[0] - ARROW_HEAD_WIDTH / 2) +
-        "px, " +
-        ARROW_ORIGIN[1] +
-        "px, " +
-        ARROW_ORIGIN[2] +
-        "px) rotateX(90deg)"
-    );
-    parent.appendChild(headDiv);
-
-    const render = function () {
-      const angle = Math.atan2(arrow[2], arrow[0]);
-
-      const arrowLength = lengthOfVector(arrow);
-
-      shaftDiv.style.height =
-        arrowLength - ARROW_HEAD_HEIGHT + 1 + ARROW_OFFSET + "px";
-      setTransform(
-        shaftDiv,
-        "translate3d(" +
-          (ARROW_ORIGIN[0] - ARROW_SHAFT_WIDTH / 2) +
-          "px, " +
-          ARROW_ORIGIN[1] +
-          "px, " +
-          ARROW_ORIGIN[2] +
-          "px) rotateX(90deg) rotateZ(" +
-          (angle - Math.PI / 2) +
-          "rad) translateY(" +
-          -ARROW_OFFSET +
-          "px)"
-      );
-      setTransform(
-        headDiv,
-        "translate3d(" +
-          (ARROW_ORIGIN[0] - ARROW_HEAD_WIDTH / 2) +
-          "px, " +
-          ARROW_ORIGIN[1] +
-          "px, " +
-          ARROW_ORIGIN[2] +
-          "px) rotateX(90deg) rotateZ(" +
-          (angle - Math.PI / 2) +
-          "rad) translateY(" +
-          (arrowLength - ARROW_HEAD_HEIGHT - 1) +
-          "px)"
-      );
-    };
-
-    this.update = function (mouseX, mouseZ) {
-      arrow = [mouseX, 0, mouseZ];
-      subtractFromVector(arrow, arrow, ARROW_ORIGIN);
-
-      const arrowLength = lengthOfVector(arrow);
-      if (arrowLength > MAX_WIND_SPEED * WIND_SCALE) {
-        multiplyVectorByScalar(
-          arrow,
-          arrow,
-          (MAX_WIND_SPEED * WIND_SCALE) / arrowLength
-        );
-      } else if (lengthOfVector(arrow) < MIN_WIND_SPEED * WIND_SCALE) {
-        multiplyVectorByScalar(
-          arrow,
-          arrow,
-          (MIN_WIND_SPEED * WIND_SCALE) / arrowLength
-        );
-      }
-
-      addToVector(tip, ARROW_ORIGIN, arrow);
-
-      render();
-
-      valueX = arrow[0] / WIND_SCALE;
-      valueY = arrow[2] / WIND_SCALE;
-    };
-
-    this.getValue = function () {
-      return lengthOfVector(arrow) / WIND_SCALE;
-    };
-
-    this.getValueX = function () {
-      return valueX;
-    };
-
-    this.getValueY = function () {
-      return valueY;
-    };
-
-    this.distanceToTip = function (vector) {
-      return distanceBetweenVectors(tip, vector);
-    };
-
-    this.getTipZ = function () {
-      return tip[2];
-    };
-
-    render();
   }
 }
 
